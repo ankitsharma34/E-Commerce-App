da9e48c0ba56f04d708abd9aeda0c17102a9023b
ankitsharma34 <ankitsharma04.iiits@gmail.com>
2026-03-14 14:27:19 +0530

    Initial Setup of Server done

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+// app config
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// API endpoints
+app.get("/", (req, res) => {
+  res.send("API working");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on PORT: ${PORT}`);
+});
