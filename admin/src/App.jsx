6df9f6cc21dbe322c055265b843d272ba1954066
ankitsharma34 <ankitsharma04.iiits@gmail.com>
2026-03-16 16:03:40 +0530

    admin - routes to pages added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add";
+import Orders from "./pages/Orders";
+import List from "./pages/List";
 
 const App = () => {
   return (
@@ -11,6 +15,11 @@ const App = () => {
       </>
       <div className="flex w-full">
         <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
       </div>
     </div>
   );
