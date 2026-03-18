50093e9257d4b60feef011e5aabea81855e3aab3
ankitsharma34 <ankitsharma04.iiits@gmail.com>
2026-03-18 13:08:14 +0530

    AddToCart controller created

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -1,5 +1,38 @@
+import userModel from "../models/userModel";
+
 // add product to user cart
-const addToCart = async (req, res) => {};
+const addToCart = async (req, res) => {
+  try {
+    const { userId, itemId, size } = req.body;
+    if (!userId || !itemId || !size) {
+      return res.json({ success: false, message: "Missing required fields" });
+    }
+
+    const userData = await userModel.findById(userId);
+    if (!userData) {
+      return res.json({ success: false, message: "User not found" });
+    }
+
+    let cartData = userData.cartData || {};
+
+    if (cartData[itemId]) {
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
+      }
+    } else {
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+
+    await userModel.findByIdAndUpdate(userId, { cartData });
+    return res.json({ success: true, message: "Added to Cart" });
+  } catch (error) {
+    console.error(error);
+    return res.json({ success: false, message: error.message });
+  }
+};
 
 // update user cart
 const updateCart = async (req, res) => {};
