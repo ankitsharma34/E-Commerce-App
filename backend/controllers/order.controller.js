1d78a2584674f6f13d3cb9423a09f8a757b7653d
ankitsharma34 <ankitsharma04.iiits@gmail.com>
2026-03-18 17:03:19 +0530

    Place Order using COD method controller created

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -1,5 +1,32 @@
+import orderModel from "../models/order.model.js";
+import userModel from "../models/userModel.js";
+
 // placing order using COD method
-const placeOrder = async (req, res) => {};
+const placeOrder = async (req, res) => {
+  try {
+    const { userId, items, amount, address } = req.body;
+
+    const orderData = {
+      userId,
+      items,
+      address,
+      amount,
+      paymentMethod: "COD",
+      payment: false,
+      date: Date.now(),
+    };
+
+    const newOrder = new orderModel(orderData);
+    await newOrder.save();
+
+    await userModel.findByIdAndUpdate(userId, { cartData: {} });
+
+    return res.json({ success: true, message: "Order Placed" });
+  } catch (error) {
+    console.error(error);
+    return res.json({ success: false, message: error.message });
+  }
+};
 
 // placing order using Stripe method
 const placeOrderStripe = async (req, res) => {};
