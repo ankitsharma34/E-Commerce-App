4de0c55e25025fb58ddde2a4347089a25152489c
ankitsharma34 <ankitsharma04.iiits@gmail.com>
2026-03-21 23:31:08 +0530

    Stripe gateway initialized and place order controller created

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -1,5 +1,6 @@
 import orderModel from "../models/order.model.js";
 import userModel from "../models/userModel.js";
+import Stripe from "stripe";
 
 // placing order using COD method
 const placeOrder = async (req, res) => {
@@ -28,8 +29,78 @@ const placeOrder = async (req, res) => {
   }
 };
 
+// initialize stripe gateway when called
+let stripeInstance = null;
+const getStripe = () => {
+  if (!stripeInstance) {
+    stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY);
+  }
+  return stripeInstance;
+};
+
 // placing order using Stripe method
-const placeOrderStripe = async (req, res) => {};
+const placeOrderStripe = async (req, res) => {
+  try {
+    const stripe = getStripe(); // initialize stripe lazily
+    const { userId, items, amount, address } = req.body;
+    const { origin } = req.headers;
+
+    // 1. Save order to database
+    const orderData = {
+      userId,
+      items,
+      address,
+      amount,
+      paymentMethod: "Stripe",
+      payment: false, // -> payment is not done yet
+      date: Date.now(),
+    };
+
+    const newOrder = new orderModel(orderData);
+    await newOrder.save();
+    // order saved in database
+
+    // 2. Build line items for Stripe Checkout
+    // Stripe needs items in a specific format
+    const line_items = items.map((item) => ({
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: item.price * 100, // Stripe uses cents, not dollars
+      },
+      quantity: item.quantity,
+    }));
+
+    // 3. Add delivery fee as a line item
+    line_items.push({
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: "Delivery Fee",
+        },
+        unit_amount: 10 * 100, // $10 delivery fee → 1000 cents
+      },
+      quantity: 1,
+    });
+
+    // 4. create stripe checkout session
+    const session = await stripe.checkout.sessions.create({
+      success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`, // Where to redirect after SUCCESSFUL payment
+      cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`, // Where to redirect if customer CANCELS payment
+      line_items, // The items to display on Stripe's checkout page
+      mode: "payment",
+      // "payment" = one-time payment
+      // "subscription" = recurring payments
+    });
+
+    return res.json({ success: true, session_url: session.url });
+  } catch (error) {
+    console.error(error);
+    return res.json({ success: false, message: error.message });
+  }
+};
 
 // placing order using Razorpay method
 const placeOrderRazorpay = async (req, res) => {};
